6812789fa31794f6284ad107538ca454d0deeae3
wamaithajoy <joyywamaitha@gmail.com>
2022-05-08 02:17:21 +0300

    first commit

---
+++ py.py
@@ -0,0 +1,19 @@
+from cgi import print_arguments
+from re import X
+from tempfile import tempdir
+
+
+print("Hello there! Welcome")
+print(len(input("What is your name ")))
+input("How old are you ")
+input("What are your expectations ")
+print("Thank your response has been received")
+
+print("Welcome to band name generator")
+city=input("Which city did you grow up in?")
+pet=input("What is the name of your pet")
+print("Your band name could be" +city + " " + pet)
+
+
+
+
